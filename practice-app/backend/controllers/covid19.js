b11b6695d2dd6266a4ef954744d58669c3335ced
emre <emregirgin171@gmail.com>
2020-05-12 22:40:26 +0300

    endpoint and unit test are implemented and tested

---
+++ practice-app/backend/controllers/covid19.js
@@ -0,0 +1,61 @@
+const Response = require('../utils/response');
+const Request = require('../utils/request');
+const Constants = require('../constants');
+
+/**
+ * Get the lastest update on the numbers of Coronovirüs cases in Turkey, 
+ * using 'covid19api.com'
+ * 
+ * Response: {
+    "status": {
+        "success": true,
+        "code": 200
+    },
+    "error": false,
+    "data": {
+        "NewConfirmed": 1114,
+        "TotalConfirmed": 139771,
+        "NewDeaths": 55,
+        "TotalDeaths": 3841,
+        "NewRecovered": 3089,
+        "TotalRecovered": 95780,
+        "ActiveCases": 40150,
+        "lastUpdate": "2020-05-12"
+    }
+}
+ */
+module.exports.getCovid19tr = function (request, response) {
+    try {
+        Request.send({ // Send request to the covid19 api.
+            'method': 'GET',
+            'url': 'https://api.covid19api.com/summary',
+            'headers': {}
+        }, async function (err, res, body) {
+            if (err || body.error) { // If there is an error respond with the error.
+                return Response.handleError(response, err || body.error);
+            }
+            
+            //console.log("Your response's body: ", body);
+
+            var dataTR = (body.Countries)[171]
+
+
+            // Set the response body.
+            const responseBody = dataTR.CountryCode == "TR" ? {
+                NewConfirmed : dataTR.NewConfirmed,
+                TotalConfirmed : dataTR.TotalConfirmed,
+                NewDeaths : dataTR.NewDeaths,
+                TotalDeaths : dataTR.TotalDeaths,
+                NewRecovered : dataTR.NewRecovered,
+                TotalRecovered : dataTR.TotalRecovered,
+                ActiveCases : dataTR.TotalConfirmed - (dataTR.TotalRecovered + dataTR.TotalDeaths),
+                lastUpdate : dataTR.Date.substring(0,10)
+            } : {};
+
+            // Respond to the front-end.
+            Response.send(response, responseBody);
+        });
+    } catch (error) {
+        Response.handleError(response, error);
+    }
+};
--- practice-app/backend/routes.js
+++ practice-app/backend/routes.js
@@ -1,7 +1,10 @@
 const nearbyVendors = require('./controllers/nearestVendor');
+const covid19TR = require('./controllers/covid19');
+
 
 // Initialize the routes.
 module.exports.initialize = function (app) {
     // Redirect the GET /vendor/nearest request to the getNearestVendor function.
     app.get("/vendor/nearest", nearbyVendors.getNearestVendor);
+    app.get("/covid19", covid19TR.getCovid19tr);
 }
