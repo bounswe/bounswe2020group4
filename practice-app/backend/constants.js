4905ac624459548a557e0e94e9a5eea448034c4a
Erdinç <m.erdincoguz@gmail.com>
2020-05-14 00:58:14 +0300

    Add the API-Key and the route for the api.

--- practice-app/backend/constants.js
+++ practice-app/backend/constants.js
@@ -1,5 +1,7 @@
 module.exports = {
     PORT: 9876,
-    GOOGLE_API_KEY: 'AIzaSyAUxl5PEaqjVomNYol-629wGMeEq56XJu8',
-    MONGO_URL: 'mongodb://localhost/practice-app'
+    GOOGLE_API_KEY: 'AIzaSyD4v7poRQslAcZAZ6yQHbz9ZzOzggHv4Ak',
+    MONGO_URL: 'mongodb://localhost/practice-app',
+    SPOTIFY_CLIENT_ID: '4818f350e39e4fe9b42471f61397b29c',
+    SPOTIFY_CLIENT_SECRET: 'dd4ea12bf5e3463493d5c5f74f9d93c2'
 }
--- practice-app/backend/routes.js
+++ practice-app/backend/routes.js
@@ -1,7 +1,10 @@
 const nearbyVendors = require('./controllers/nearestVendor');
+const randomPlaylists = require('./controllers/randomCoronaPlaylist');
 
 // Initialize the routes.
 module.exports.initialize = function (app) {
     // Redirect the GET /vendor/nearest request to the getNearestVendor function.
     app.get("/vendor/nearest", nearbyVendors.getNearestVendor);
+    // Redirect the GET /randomPlaylist to the randomCoronaPlaylist function.
+    app.get("/random-playlist", randomPlaylists.getRandomCoronaPlaylist);
 }
