dfcb4d90991e4d5506a591dbaf490b28102304dc
eylulyalcinkaya <eylulyalcnkaya@gmail.com>
2020-05-18 19:11:43 +0300

    Added quote endpoint

---
+++ practice-app/backend/controllers/quoteOfTheDay.js
@@ -0,0 +1,45 @@
+const Response = require('../utils/response');
+const Request = require('../utils/request');
+const Constants = require('../constants');
+
+/**
+ * Get a random quote from the quote API.
+ * Response: {
+ *    status: {
+ *        success: true,
+ *        code: 200
+ *    }, data: {
+ *			quote: '...',
+ *			author: '...'
+ *    }
+ *}
+ */
+
+module.exports.getQuote = async function (request, response) {
+    try {
+        Request.send({ //
+            url: 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?',
+            method: 'POST',
+            body: {}
+        }, async function (err, res, body) {
+            if (err || body.error) { // If there is an error respond with the error.
+                return Response.handleError(response, err || body.error);
+            }
+            console.log("Response body: ", body);
+
+            body = body.substring(2, body.length - 1)
+            bodyJson = JSON.parse(body)
+
+            // Set the response body.
+            const responseBody = {
+                quote: bodyJson["quoteText"],
+                author: bodyJson["quoteAuthor"]
+            }
+
+            // Respond to the front-end.
+            Response.send(response, responseBody);
+        });
+    } catch (error) {
+        Response.handleError(response, error);
+    }
+};
--- practice-app/backend/routes.js
+++ practice-app/backend/routes.js
@@ -1,7 +1,13 @@
 const nearbyVendors = require('./controllers/nearestVendor');
+const quoteOfTheDay = require('./controllers/quoteOfTheDay');
+
 
 // Initialize the routes.
 module.exports.initialize = function (app) {
     // Redirect the GET /vendor/nearest request to the getNearestVendor function.
     app.get("/vendor/nearest", nearbyVendors.getNearestVendor);
+
+    // Redirect GET /quote request to getQuote function
+    app.get("/quote", quoteOfTheDay.getQuote);
+
 }
