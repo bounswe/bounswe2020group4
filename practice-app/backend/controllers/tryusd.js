93fa32a3663bc4d5616caf8e13a0fc74dd43e348
olcaytoturker <43935275+olcaytoturker@users.noreply.github.com>
2020-05-18 15:26:10 +0300

    Add files via upload

---
+++ practice-app/backend/controllers/tryusd.js
@@ -0,0 +1,40 @@
+const Response = require('../utils/response');
+const Request = require('../utils/request');
+/**
+ * Explanation
+ * Response: {
+ *    status: {
+ *        success: true,
+ *        code: 200
+ *    }, data: {
+ *    }
+ *}
+ */
+module.exports.getTryUsd =  function (request, response) {
+    try {
+        Request.send({ // Send request to the google api.
+            url: 'https://prime.exchangerate-api.com/v5/' + process.env.TRY_USD_API_KEY  + '/latest/USD',
+            method: 'POST',
+            body: {}
+        }, async function (err, res, body) {
+            if (err || body.error) { // If there is an error respond with the error.
+                return Response.handleError(response, err || body.error);
+            }
+
+            var myResponseParam = body.conversion_rates.TRY;
+
+            // Set the response body.
+            const responseBody = {
+
+                TRY: myResponseParam
+
+            }
+
+        
+            // Respond to the front-end.
+            Response.send(response, responseBody);
+        });
+    } catch (error) {
+        Response.handleError(response, error);
+    }
+};
