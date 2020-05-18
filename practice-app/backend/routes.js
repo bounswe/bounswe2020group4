a3a916fd523bb6dcb1c320b09a47758c7f48bdc1
burak <burakcuhadar96@gmail.com>
2020-05-18 20:43:07 +0300

    Implement Covid-19 News Endpoint

--- practice-app/backend/routes.js
+++ practice-app/backend/routes.js
@@ -1,7 +1,14 @@
 const nearbyVendors = require('./controllers/nearestVendor');
+const covidNews = require('./controllers/covidNews');
 
 // Initialize the routes.
 module.exports.initialize = function (app) {
     // Redirect the GET /vendor/nearest request to the getNearestVendor function.
     app.get("/vendor/nearest", nearbyVendors.getNearestVendor);
 }
+
+module.exports.initialize = function (app) {
+    // Redirect the GET /vendor/nearest request to the getNearestVendor function.
+    app.get("/covid/news", covidNews.getNews);
+}
+
