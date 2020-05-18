eafaa22ddb0d967e60f19d02cb9d3d8a60f712f3
rooster20 <43935275+rooster20@users.noreply.github.com>
2020-05-18 23:02:47 +0300

    Try/Usd rate end point.

--- practice-app/backend/routes.js
+++ practice-app/backend/routes.js
@@ -1,7 +1,8 @@
 const nearbyVendors = require('./controllers/nearestVendor');
-
+const tryToUsd = require('./controllers/tryusd');
 // Initialize the routes.
 module.exports.initialize = function (app) {
     // Redirect the GET /vendor/nearest request to the getNearestVendor function.
     app.get("/vendor/nearest", nearbyVendors.getNearestVendor);
+    app.get("/tryusd", tryToUsd.getTryUsd);
 }
