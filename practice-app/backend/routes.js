1283b8ad79c707474edab93bb425dc01516f0b0b
olcaytoturker <43935275+olcaytoturker@users.noreply.github.com>
2020-05-18 15:49:06 +0300

    Update routes.js

--- practice-app/backend/routes.js
+++ practice-app/backend/routes.js
@@ -4,6 +4,5 @@ const tryToUsd = require('./controllers/tryusd');
 module.exports.initialize = function (app) {
     // Redirect the GET /vendor/nearest request to the getNearestVendor function.
     app.get("/vendor/nearest", nearbyVendors.getNearestVendor);
-    // Redirect the GET /tryusd request to the getTryUsd function.
-    app.get("/tryusd", tryToUsd.getTryUsd);
+    app.get("/tryusd", tryToUsd.getTryUsd); // Redirect the GET /tryusd request to the getTryUsd function.
 }
