2d364a9c27b52282780dba04dd4fb66b18543123
olcaytoturker <43935275+olcaytoturker@users.noreply.github.com>
2020-05-18 15:27:18 +0300

    Add files via upload

---
+++ practice-app/backend/tests/tryusd.spec.js
@@ -0,0 +1,35 @@
+const chai = require('chai'),
+    mocha = require('mocha'),
+    app = require('../app').App,
+    request = require('supertest'),
+    describe = mocha.describe,
+    it = mocha.it;
+const Constants = require('./constants');
+
+describe('# Nearest Vendor', function () {
+    it("should return a vendor object", async function () {
+        const response = await request(app)
+            .get("/tryusd");
+
+        chai.expect(response.body.status).to.be.an('object');
+        chai.expect(response.body.status.success).to.equal(true);
+        chai.expect(response.body.status.code).to.equal(200);
+        chai.expect(response.body.data).to.be.an('object');
+        chai.expect(response.body.data.TRY).to.be.a('number');
+
+    });
+
+    it("should respond with googleapi token error", async function () {
+    process.env.TRYUSD_API_KEY = "wrong-api-key";
+
+    const response = await request(app)
+        .get("/tryusd");
+
+    chai.expect(response.body.status).to.be.an('object');
+    chai.expect(response.body.status.success).to.equal(false);
+    chai.expect(response.body.status.code).to.equal(404);
+    chai.expect(response.body.data).to.equal(undefined);
+    chai.expect(response.body.error).to.equal(undefined);
+});
+
+});
