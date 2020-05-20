c42a27580dbb522f340990dcc121361f931218fd
zeritte <zeritte@gmail.com>
2020-05-20 04:29:28 +0300

    tests

--- practice-app/backend/tests/nearestHospital.spec.js
+++ practice-app/backend/tests/nearestHospital.spec.js
@@ -15,47 +15,48 @@ describe("# Nearest Hospital Names", function () {
     chai.expect(response.body.data).to.be.an("object");
     chai.expect(response.body.data.names).to.be.an("array");
     chai.expect(response.body.data.count).to.be.an("number");
-
-    response.body.data.names.forEach(function (name) {
-      chai.expect(name).to.be.a("string");
-    });
   });
   it("should return a list of nearest hospitals to Bogazici Uni", async function () {
-    const response = await request(app).get("/nearesthospitals", {
-      lat: 41.08542,
-      long: 29.04885,
+    const response = await request(app).get("/nearesthospitals").send({
+      lat: 41.0862,
+      long: 29.0444,
       radius: 2000
     });
 
     if (response.body.data.count) {
-      // make sure request limit is not exceeded
+      // make sure request limit is not exceeded by checking count
+      response.body.data.names.forEach(function (name) {
+        chai.expect(name).to.be.a("string");
+      });
       chai
-        .expect(response.body.data.names)
-        .to.be.an("array")
-        .that.includes("Baltalimanı Family Health Center");
-      chai
-        .expect(response.body.data.names)
-        .to.be.an("array")
-        .that.includes("Acıbadem Etiler Medical Center");
+        .expect(
+          ["Baltalimanı Family Health Center", "Üsküdar University NP Etiler Medical Center"].every(
+            (e) => response.body.data.names.indexOf(e) >= 0
+          )
+        )
+        .to.equal(true);
     }
   });
   it("should return a list of nearest hospitals to Besiktas", async function () {
-    const response = await request(app).get("/nearesthospitals", {
+    const response = await request(app).get("/nearesthospitals").send({
       lat: 41.0422,
       long: 29.0067,
       radius: 2000
     });
 
     if (response.body.data.count) {
-      // make sure request limit is not exceeded
+      // make sure request limit is not exceeded by checking count
+      response.body.data.names.forEach(function (name) {
+        chai.expect(name).to.be.a("string");
+      });
       chai
-        .expect(response.body.data.names)
-        .to.be.an("array")
-        .that.includes("Nisantasi Family Health Center");
-      chai
-        .expect(response.body.data.names)
-        .to.be.an("array")
-        .that.includes("T. C. The Ministry of Health Sinanpaşa Family Health Center");
+        .expect(
+          [
+            "T. C. The Ministry of Health Sinanpaşa Family Health Center",
+            "Nisantasi Family Health Center"
+          ].every((e) => response.body.data.names.indexOf(e) >= 0)
+        )
+        .to.equal(true);
     }
   });
 });
