5bcf2efe951b9b080921786668aef05d1a147aff
Cagri Ciftci <cagri.ciftci@solidict.com>
2020-05-23 21:41:30 +0300

    add weather-important-cities scripts and containers

--- practice-app/backend/controllers/weatherImportantCities.js
+++ practice-app/backend/controllers/weatherImportantCities.js
@@ -52,7 +52,7 @@ module.exports.getCurrentWeathers = function (request, response) {
         ******
         */
         Request.send({ // Sends request to openweather api.
-            url: 'http://api.openweathermap.org/data/2.5/group?id=745042,323784,311044&units=metric&lang=tr&appid=' + process.env.OPENWEATHER_API_KEY,
+            url: 'http://api.openweathermap.org/data/2.5/group?id=745042,323784,311044&units=metric&appid=' + process.env.OPENWEATHER_API_KEY,
             method: 'GET',
             body: {}
         }, async function (err, res, body) {
