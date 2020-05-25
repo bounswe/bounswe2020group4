8336e99665ffb619cd94d7840abaefa75b18c46f
emre <emregirgin171@gmail.com>
2020-05-26 00:15:46 +0300

    bash script

---
+++ practice-app/commands.sh
@@ -0,0 +1,11 @@
+# make sure that docker is installed and logged in 
+# run the following commands in the directory where docker-compose.yml is present
+
+pip3 install docker-compose
+
+docker-compose pull
+
+#this process may take a while
+
+docker-compose up
+
