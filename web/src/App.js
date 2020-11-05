b63f4bc18c69215d10ff5e28a7e58565629d1368
Meriç Üngör <meric@Merics-MacBook-Pro.local>
2020-11-05 22:50:32 +0300

    Setup history, header, Routes

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Router, Route } from 'react-router-dom';
+import history from './util/history';
+
+import Header from './components/Header';
+
 
 const App = () => {
-
     return (
-        <div>
-            APP
+        <div className='page-container'>
+            <Router history={history}>
+                <div>
+                    <Header />
+                </div>
+            </Router>
         </div>
     );
 }
