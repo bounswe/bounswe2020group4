30e71f4d9d4deea25dd34ac0ca21bcadec01ad57
Cagri Ciftci <cagri.ciftci@solidict.com>
2020-11-12 00:56:57 +0300

    (AND) Base App Setup #120
    Retrofit added. Api connection structure added(ConnectionManager and NetworkServiceWrapper)

---
+++ android/Buyo/app/src/main/java/com/cmpe352group4/buyo/api/Api.kt
@@ -0,0 +1,26 @@
+package com.cmpe352group4.buyo.api
+
+import androidx.lifecycle.LiveData
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface Api {
+
+    // These are some example to define endpoints
+
+//    @GET("products")
+//    fun fetchDailyProgram(): LiveData<ApiResponse<ProductList>>
+//
+//    @GET("product")
+//    fun getProductDetail(
+//        @Query("id") productId: Int
+//    ): LiveData<ApiResponse<Product>>
+//
+//    @GET("product/{id}")
+//    fun fetchProductDetail(@Path("id") productId:Int): LiveData<ApiResponse<Product>>
+
+
+
+
+}
---
+++ android/Buyo/app/src/main/java/com/cmpe352group4/buyo/util/GeneralUtil.kt
@@ -0,0 +1,46 @@
+package com.cmpe352group4.buyo.util
+
+import android.app.AlertDialog
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+
+class GeneralUtil {
+    companion object{
+
+        fun checkNetworkConnection(context: Context?): Boolean {
+            if (context == null)
+                return false
+            var result = false
+            val connectivityManager =
+                context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                val networkCapabilities = connectivityManager.activeNetwork ?: return false
+                val actNw =
+                    connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
+                result = when {
+                    actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                    actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                    actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                    else -> false
+                }
+            } else {
+                connectivityManager.run {
+                    connectivityManager.activeNetworkInfo?.run {
+                        result = when (type) {
+                            ConnectivityManager.TYPE_WIFI -> true
+                            ConnectivityManager.TYPE_MOBILE -> true
+                            ConnectivityManager.TYPE_ETHERNET -> true
+                            else -> false
+                        }
+
+                    }
+                }
+            }
+
+            return result
+        }
+
+    }
+}
