9a3d0ac79610e94d0a03fe069357d1e7c30fd5f5
Cagri Ciftci <cagri.ciftci@solidict.com>
2020-11-12 02:51:36 +0300

    (AND) Navigation Bar and Manager #123
    Navigation Bar UI and functionality added.
    Some extensions(utility functions) added.

---
+++ android/Buyo/app/src/main/java/com/cmpe352group4/buyo/util/extensions/GeneralExtensions.kt
@@ -0,0 +1,21 @@
+package com.cmpe352group4.buyo.util.extensions
+
+import android.widget.ImageView
+import com.bumptech.glide.request.RequestOptions
+
+ // It doesn't find GlideApp, find out the problem later
+
+//fun ImageView.loadFromURL(url: String) {
+//    GlideApp.with(this)
+//        .load(url)
+//        .centerCrop()
+//        .fitCenter()
+//        .into(this)
+//}
+//
+//fun ImageView.loadFromURLWithCircle(url: String) {
+//    GlideApp.with(this)
+//        .load(url)
+//        .apply(RequestOptions.circleCropTransform())
+//        .into(this)
+//}
---
+++ android/Buyo/app/src/main/java/com/cmpe352group4/buyo/widgets/NoSwipePager.kt
@@ -0,0 +1,17 @@
+package com.cmpe352group4.buyo.widgets
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import androidx.viewpager.widget.ViewPager
+
+class NoSwipePager: ViewPager {
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+
+    override fun onInterceptTouchEvent(ev: MotionEvent?): Boolean = false
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(ev: MotionEvent?): Boolean = false
+}
