faa9269c271e8653b5de7be126f645797eec3d64
eylulyalcinkaya <eylulyalcnkaya@gmail.com>
2020-11-16 00:17:10 +0300

    Sign up and sign in pages -  UI

--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Router, Route } from 'react-router-dom';
 import history from './util/history';
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 
 import Header from './components/Header';
 
@@ -11,6 +13,9 @@ const App = () => {
             <Router history={history}>
                 <div>
                     <Header />
+                    <Route path="/signin" exact component={SignIn} />
+                    <Route path="/signup" exact component={SignUp} />
+
                 </div>
             </Router>
         </div>
