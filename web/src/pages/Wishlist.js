0a09715dadeeba856da2c4a2fe90a5e31ef8fc57
burak <burakcuhadar96@gmail.com>
2020-11-20 12:28:13 +0300

    Add wishlist page

---
+++ web/src/pages/Wishlist.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import ProductCard from '../components/ProductCard'
+
+import './Wishlist.css'
+
+const Wishlist = () => {
+  return(
+    <div className='wishlist-container'>
+      <div className='list-title' >List: Wishlist(All)</div>
+      <div className='list-container'>
+        <div className='list-links'>
+          <h3>Lists</h3>
+        </div>
+        <div className='product-card-container'>
+          <ProductCard name='Product' price='$7.99'/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Wishlist
