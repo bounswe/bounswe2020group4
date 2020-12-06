7931aeb27a2cac5fbe318ce0a3bf3f3228c3a83e
olcaytoturker <43935275+olcaytoturker@users.noreply.github.com>
2020-12-06 17:38:08 +0300

    Profile Info Endpoint implementation

--- backend/account_service/views/account.js
+++ backend/account_service/views/account.js
@@ -2,6 +2,34 @@ const Customer = require("../models/customer").Customer;
 const Vendor = require("../models/vendor").Vendor;
 const Counter = require("../models/counter").Counter;
 
+module.exports.getAccountInfo = async (params) => {
+  let account;
+  const collection = params.userType === "customer" ? Customer : Vendor;
+  try {
+    account = await collection.findOne({ id: params.id });
+
+    if (account && params.userType === "customer") {
+
+      account = account.toJSON();
+      delete account._id;
+      delete account.__v;
+      delete account.password;
+
+    } else if(account && params.userType === "vendor"){
+
+      account = account.toJSON();
+      delete account._id;
+
+    }
+
+    return account;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
+
+
 module.exports.login = async (params) => {
   try {
     const collection = params.userType === "customer" ? Customer : Vendor;
