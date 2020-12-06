4bdbd7db01b06929987e85b92effc091a896f7c4
koraycetiin <koraycetin67@gmail.com>
2020-12-06 18:10:20 +0300

    remove false comments, change nginx set

--- backend/product_service/models/comment.js
+++ backend/product_service/models/comment.js
@@ -1,9 +1,5 @@
 const mongoose = require("mongoose");
 
-/**
- * Schema of a Vendor object in the database, vendor documents resides
- * in 'vendors' collection.
- */
 module.exports.Comment = mongoose.model(
   "Comment",
   new mongoose.Schema({
