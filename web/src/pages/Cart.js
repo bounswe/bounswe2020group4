d1f2a6cbbf23c23f1ebb42769b0ab8773f1dfb5c
eylulyalcinkaya <eylulyalcnkaya@gmail.com>
2020-12-12 19:20:04 +0300

    Cart page breadcrumb header

--- web/src/pages/Cart.js
+++ web/src/pages/Cart.js
@@ -1,15 +1,16 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import history from '../util/history'
 import { connect } from 'react-redux'
+
 import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 
-
 import DefaultProductImg from '../images/default-product-image.png'
 
 import './Cart.css'
+import './Checkout.css'
 import CartProduct from '../components/CartProduct'
 import Container from 'react-bootstrap/esm/Container'
 
@@ -51,27 +52,30 @@ const Cart = (props) => {
     ]
 
     return (
-    <Container fluid>
-        <Row>
-        <Col>
-        {products.map(p =>
-            <Container className="cart-product">
-                <CartProduct name={p.name} price={p.price} imgUrl={p.imgUrl | DefaultProductImg} productId={p.productId} vendorName={p.vendorName} brand={p.brand} size={p.size} amount={p.amount}/>
-            </Container>
-        )}
-        </Col>
-        <Col sm={2} className="align-self-center text-center">
-            <Container className="cart-total">
-            <Card >
-                <Card.Header className="header">Cart Total</Card.Header>
-                <Card.Body>{cartTotal}</Card.Body>
-                <Button className="checkout-button" onClick={redirectToCheckout}>Checkout</Button>
-            </Card>
-            </Container>
-            </Col>
-        </Row>
-            
-    </Container>
+    <div>
+        <div className="checkout-header-container px-5 py-3">Cart</div>
+        <Container fluid>
+            <Row>
+                <Col>
+                {products.map(p =>
+                    <Container className="cart-product">
+                        <CartProduct name={p.name} price={p.price} imgUrl={p.imgUrl | DefaultProductImg} productId={p.productId} vendorName={p.vendorName} brand={p.brand} size={p.size} amount={p.amount}/>
+                    </Container>
+                )}
+                </Col>
+                <Col sm={2} className="align-self-center text-center">
+                    <Container className="cart-total">
+                        <Card >
+                            <Card.Header className="header">Cart Total</Card.Header>
+                            <Card.Body>{cartTotal}</Card.Body>
+                            <Button className="checkout-button" onClick={redirectToCheckout}>Checkout</Button>
+                        </Card>
+                    </Container>
+                </Col>
+            </Row>
+                
+        </Container>
+    </div>
     )
 }
 
