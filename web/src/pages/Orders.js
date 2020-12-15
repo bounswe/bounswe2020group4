fc0470ac4d990efe680d141fa6a4570da318d853
burak <burakcuhadar96@gmail.com>
2020-12-15 11:57:06 +0300

    Add comment button and dialog to orders page

--- web/src/pages/Orders.js
+++ web/src/pages/Orders.js
@@ -5,12 +5,19 @@ import OrderDetails from '../components/OrderDetails'
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Accordion from '@material-ui/core/Accordion';
+import { makeStyles } from '@material-ui/core/styles';
+
 import AccordionDetails from '@material-ui/core/AccordionDetails';
 import AccordionSummary from '@material-ui/core/AccordionSummary';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
-
 import "./Orders.css"
+
+const useStyles = makeStyles((theme) => ({
+  indicator: {
+    backgroundColor: 'red',
+  },
+}))
 
 const TabPanel = (props) => {
   const { children, value, index, ...other } = props;
@@ -37,6 +44,7 @@ const Orders = () => {
   const [value, setValue] = useState(0);
   const [expanded, setExpanded] = useState(false);
 
+  const classes = useStyles();
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -52,7 +60,7 @@ const Orders = () => {
         value={value}
         onChange={handleChange}
         indicatorColor="primary"
-        textColor="primary"
+        classes={{indicator: classes.indicator}}
         centered >
         <Tab label="Paid" id="tab-1"/>
         <Tab label="Delivered" id="tab-2"/>
@@ -76,7 +84,22 @@ const Orders = () => {
         </Accordion>
       </TabPanel>
       <TabPanel value={value} index={1}>
-        Item two
+        <Accordion expanded={expanded === 'panel2'} onChange={handleExpand('panel2')}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel2bh-content"
+            id="panel2bh-header"
+          >
+            <div className="order-heading">
+              <div>Order No: 123667</div>
+              <div>Date:10.12.2020</div>
+              <div>Total: 110 tl</div>
+            </div>
+          </AccordionSummary>
+          <AccordionDetails>
+            <OrderDetails isDelivered="true" />
+          </AccordionDetails>
+        </Accordion>
       </TabPanel>
     </div>
   )
