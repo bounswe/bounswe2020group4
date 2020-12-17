49250e2fb2c620e40cf15623eb03a7b35373b11c
koraycetiin <koraycetin67@gmail.com>
2020-12-17 23:05:32 +0300

    add comments to the interaction service

--- backend/interaction_service/views/like.js
+++ backend/interaction_service/views/like.js
@@ -1,6 +1,16 @@
 const Like = require("../models/like").Like;
 const ObjectId = require("mongoose").Types.ObjectId;
 
+/**
+ * If a like exists from given customer to the given product, removes it
+ * If it's not, adds a like to given product from given customer.
+ * @param {
+ *  customerId: String,
+ *  productId: String
+ * } params
+ *
+ * @returns {true|error}
+ */
 module.exports.like = async (params) => {
   try {
     const isLiked = await Like.countDocuments({
