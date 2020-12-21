a540339aefaea716abbb8d2c8620eea41fd0bc5d
burak <burakcuhadar96@gmail.com>
2020-12-22 02:41:50 +0300

    Adjust padding on wishlist and category products pages

--- web/src/pages/Wishlist.js
+++ web/src/pages/Wishlist.js
@@ -7,39 +7,40 @@ import wishlistService from '../services/wishlist'
 import './Wishlist.css'
 
 const Wishlist = (props) => {
-	const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([])
 
-	useEffect(() => {
+  useEffect(() => {
 
-		if(!props.isLoggedIn) {
-			history.push('/signin')
-			return
-		}
+    if(!props.isLoggedIn) {
+      history.push('/signin')
+      return 
+    }
+    
+    const getWishlist = async () => {
+      const prods = await wishlistService.getWishlist(props.customerId)  
+      setProducts(prods)
+    }
 
-		wishlistService
-			.getWishlist(props.customerId)
-			.then(prods => {
-				setProducts(prods)
-			})
-	}, [props.isLoggedIn, props.customerId])
+    getWishlist()
+  }, [props.isLoggedIn, props.customerId])
 
-	return(
-		<div className='wishlist-container'>
-			<div className='list-title' >List: Wishlist(All)</div>
-			<div className='list-container'>
-				<div className='list-links'>
-					<h3>Lists</h3>
-				</div>
-				<div className='product-cards'>
-					{products.map(p =>
-						<div key={p.id} className='product-card-container'>
-							<ProductCard name={p.name} price={p.price} imgUrl={p.imageUrl} productId={p.id}/>
-						</div>
-					)}
-				</div>
-			</div>
-		</div>
-	)
+  return(
+    <div>
+      <div className='list-title px-5 py-3' >List: Wishlist(All)</div>
+      <div className='list-container'>
+        <div className='list-links'>
+          <h3>Lists</h3>
+        </div>
+        <div className='product-cards'>
+          {products.map(p => 
+            <div key={p.id} className='product-card-container'>
+              <ProductCard name={p.name} price={p.price} imgUrl={p.imageUrl} productId={p.id}/>
+            </div>          
+          )}
+        </div>
+      </div>
+    </div>
+  )
 }
 
 const mapStateToProps = (state) => {
