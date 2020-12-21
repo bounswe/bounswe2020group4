feeb7cde996852c64b4e390a4507de559f20b364
Cagri Ciftci <cagri9100@gmail.com>
2020-12-22 02:55:11 +0300

    Checkout Page UI added
    Functionalities will be implemented according to backend.

---
+++ android/Buyo/app/src/main/java/com/cmpe352group4/buyo/ui/cart/CheckoutPageFragment.kt
@@ -0,0 +1,50 @@
+package com.cmpe352group4.buyo.ui.cart
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.ViewModelProvider
+import com.cmpe352group4.buyo.R
+import com.cmpe352group4.buyo.base.BaseFragment
+import com.cmpe352group4.buyo.datamanager.shared_pref.SharedPref
+import com.cmpe352group4.buyo.viewmodel.CartViewModel
+import javax.inject.Inject
+
+
+class CheckoutPageFragment : BaseFragment() {
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    private val cartViewModel: CartViewModel by activityViewModels {
+        viewModelFactory
+    }
+
+    @Inject
+    lateinit var sharedPref: SharedPref
+
+
+    companion object {
+        fun newInstance() = CheckoutPageFragment()
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.fragment_checkout, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+
+}
