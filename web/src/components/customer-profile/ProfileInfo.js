25a508053336da4dd44e4b2fed16523dd4c8923a
Eylul Yalcinkaya <eylulyalcnkaya@gmail.com>
2020-12-25 12:47:43 +0300

    Remove birthday info from profile

--- web/src/components/customer-profile/ProfileInfo.js
+++ web/src/components/customer-profile/ProfileInfo.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import getProfileInfo from '../../services/account.js'
 
 import './ProfileInfo.css'
 
-const ProfileInfo = () => {
+const ProfileInfo = (props) => {
+
 	return (
 		<div className='container p-3'>
 			<div className='row m-3'>
@@ -13,7 +16,7 @@ const ProfileInfo = () => {
 								<label hmtlFor='name'>Name</label>
 							</div>
 							<div className='col'>
-								<input type='text' className='form-control form-control-md text-left' id='name' value='Battal'/>
+								<input type='text' className='form-control form-control-md text-left' id='name' value={firstName}'/>
 							</div>
 						</div>
 					</div>
@@ -64,23 +67,6 @@ const ProfileInfo = () => {
 							</div>
 						</div>
 					</div>
-					<div className='form-group'>
-						<div className='row'>
-							<div className='col-3'>
-								<label htmlFor='birthday'>Birthday</label>
-							</div>
-							<div className='col'>
-								<input className='form-range form-column' type='number' id='day' min='1' max='31'/>
-							</div>
-							<div className='col'>
-								<input className='form-range form-column' type='number' id='month' min='1' max='12'/>
-							</div>
-							<div className='col'>
-								<input className='form-range form-column' type='number' id='year' min='1900' max='2020'/>
-							</div>
-						</div>
-					</div>
-
 					<div className='text-right'>
 						<button type='submit' className='btn btn-danger'>Update</button>
 					</div>
@@ -91,4 +77,12 @@ const ProfileInfo = () => {
 	)
 }
 
-export default ProfileInfo
+const mapStateToProps = (state) => {
+	return {
+		isLoggedIn: state.signIn.isLoggedIn,
+		customerId: state.signIn.userId,
+		userType: state.signIn.userType
+	}
+}
+
+export default connect(mapStateToProps)(ProfileInfo)
