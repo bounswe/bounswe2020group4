502bd56a7d40e57e4cef84972cba2afe6953407c
burak <burakcuhadar96@gmail.com>
2020-12-26 15:18:22 +0300

    Get comments of a product from backend and add product description

--- web/src/components/Comments.js
+++ web/src/components/Comments.js
@@ -5,18 +5,17 @@ import './Comments.css'
 const Comment = ({user, comment}) => {
 	return(
 		<div className='comment-container'>
-			<div>{'21.11.2020 | ' + user[0] + '*****'}</div>
+			<div>{'User: ' + user[0] + '*****'}</div>
 			<p>{comment}</p>
 		</div>
 	)
 }
 
-const Comments = () => {
+const Comments = ({ comments }) => {
 	return(
 		<div className='comments-container'>
 			<h2>Comments</h2>
-			<Comment user="Burak" comment="I bought these toys for my grandson and he loved it. It is made of high-quality material." />
-			<Comment user="Ayşe" comment="The vendor was really helpful. There was an issue with shipping but it was solved pretty quickly." />
+			{comments ? comments.map(c => <Comment key={c.id} comment={c.text} user={c.owner.username}/>) : null}
 		</div>
 	)
 }
