16c26a5d968d0f608ee109be707c793e77eb5650
Cagri Ciftci <cagri9100@gmail.com>
2020-12-28 18:56:33 +0300

    Minor changes in cart Page

--- android/Buyo/app/src/main/java/com/cmpe352group4/buyo/ui/cart/CartAdapter.kt
+++ android/Buyo/app/src/main/java/com/cmpe352group4/buyo/ui/cart/CartAdapter.kt
@@ -44,11 +44,11 @@ class CartAdapter (
             var vendorInfo = "Vendor: " + modal.vendor.name + "   Rate: " + modal.vendor.rating
             var productName = "Product: " + modal.name
             var productInfo = "Brand: " + modal.brand + "\n"
-            var productQuantity = "Quantity: " + modal.productInfo[0].stockValue
-            var productPrice = "Price: " + (modal.price * modal.productInfo[0].stockValue) + "$"
+            var productQuantity = "Quantity: " + modal.quantity
+            var productPrice = "Price: " + (modal.price * modal.quantity) + "₺"
             var i = 0
-            modal.productInfo[0].attributes.forEach {
-                productInfo += if(i++ ==  modal.productInfo[0].attributes.size - 1){
+            modal.attributes.forEach {
+                productInfo += if(i++ ==  modal.attributes.size - 1){
                     it.name + ": " + it.value
                 }else{
                     it.name + ": " + it.value + "\n"
