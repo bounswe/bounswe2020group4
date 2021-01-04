d12c9602b98d94a8c4ea71bc796643fdb2869425
multiminparvo <velican2427@gmail.com>
2021-01-04 21:59:42 +0300

    Tests are added for category and product list

---
+++ backend/product_service/tests/product.spec.js
@@ -0,0 +1,69 @@
+const chai = require("chai"),
+  { describe, before, after } = require("mocha"),
+  db = require("../db/init"),
+   Product = require("../models/product").Product,
+   Vendor = require("../models/vendor").Vendor,
+   Comment = require("../models/comment").Comment,
+   Customer = require("../models/customer").Customer,
+  { ErrorMessage } = require("../constants/error");
+
+process.env.MONGO_URL = process.env.TEST_MONGO_URL;
+
+describe("# Account view tests", async function () {
+  this.timeout(0);
+
+  let customerId;
+  let vendorId;
+
+  before("Create test user", async () => {
+    await db.initialize();
+  });
+
+  describe("function: getProducts", () => {
+    it("should get products correctly", async () => {
+      const result = await Product.getProducts();
+
+      console.log(result)
+
+      chai.expect(result.success).to.be.true;
+      chai.expect(result).to.be.instanceof(Array);
+      chai.expect(result).to.have.length.above(0);
+    });
+
+    it("should fail with empty array", async () => {
+      const result = await  await Product.getProducts();
+
+      chai.expect(result.success).to.be.false;
+      chai.expect(result).to.be.instanceof(Array);
+      chai.expect(result).to.have.length.equal(0);
+    });
+
+  });
+
+
+  describe("function: getProductCategories", () => {
+    it("should get categories correctly", async () => {
+      const result = await Product.getProductCategories();
+      console.log(result)
+
+      chai.expect(result.success).to.be.true;
+      chai.expect(result).to.be.instanceof(Array);
+      chai.expect(result).to.have.length.above(0);
+    });
+
+    it("should fail with empty array", async () => {
+      const result = await  await Product.getProductCategories();
+      console.log(result)
+
+      chai.expect(result.success).to.be.false;
+      chai.expect(result).to.be.instanceof(Array);
+      chai.expect(result).to.have.length.equal(0);
+    });
+
+  });
+
+
+  after("Delete added documents", async () => {
+    await Promise.all([Customer.deleteMany(), Vendor.deleteMany(),Product.deleteMany(),Comment.deleteMany(),Customer.deleteMany()]);
+  });
+});
