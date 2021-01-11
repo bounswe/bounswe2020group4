402948f5bfe7daf9d787d0c54754418ecd1b9a75
burak <burakcuhadar96@gmail.com>
2021-01-11 18:20:48 +0300

    Add messaging UI, add filter remove option

--- web/src/App.js
+++ web/src/App.js
@@ -18,6 +18,7 @@ import VendorSignIn from './pages/VendorSignIn'
 import CustomerProfile from './pages/CustomerProfile'
 import Orders from './pages/Orders'
 import CustomerAddresses from './pages/CustomerAddresses'
+import Messages from './pages/Messages'
 
 //Styling
 import './App.css'
@@ -46,7 +47,8 @@ const App = ({ showHeader }) => {
 					<Route path="/products" exact component={CategoryProducts}/>
 					<Route path="/orders" exact component={Orders}/>
 					<Route path="/customerprofile" exact component={CustomerProfile}/>
-          <Route path="/customeraddresses" exact component={CustomerAddresses}/>
+					<Route path="/messages" exact component={Messages}/>
+          			<Route path="/customeraddresses" exact component={CustomerAddresses}/>
          </div>
        </Router>
      </div>
