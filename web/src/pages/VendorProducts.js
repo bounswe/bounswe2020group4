581204d19bf89ba11f83a8611abd170fba675d82
Eylul Yalcinkaya <eylulyalcnkaya@gmail.com>
2021-01-16 15:35:57 +0300

    Vendor products page UI implemented

--- web/src/pages/VendorProducts.js
+++ web/src/pages/VendorProducts.js
@@ -3,24 +3,99 @@ import { connect } from 'react-redux'
 import { hideHeader, showHeader, showVendorHeader, hideVendorHeader } from '../redux/actions'
 import history from '../util/history'
 
+import VendorProductCard from '../components/VendorProductCard'
+import './VendorProducts.css'
+
 
 const VendorProducts = (props) => {
 
 	useEffect(() => {
-
-		if(!props.isLoggedIn | props.userType != 'vendor') {
-			history.push('/vendorsignin')
-			return
-		}
+		
+		//TODO: uncomment this
+		//if(!props.isLoggedIn | props.userType != 'vendor') {
+		//	history.push('/vendorsignin')
+		//	return
+		//}
 
 		props.hideHeader()
 		props.showVendorHeader()
 		return () => props.showHeader()
 	}, [])
 
+	const products = [
+		{
+			'productId': 1,
+			'name': 'T-shirt',
+			'imgUrl': '',
+			'price': '50',
+			'brand': 'Koton'
+		},
+		{
+			'productId': 2,
+			'name': 'Pants',
+			'imgUrl': '',
+			'price': '70',
+			'brand': 'Koton'
+		},
+		{
+			'productId': 2,
+			'name': 'Pants',
+			'imgUrl': '',
+			'price': '70',
+			'brand': 'Koton'
+		}, 
+		{
+			'productId': 2,
+			'name': 'Pants',
+			'imgUrl': '',
+			'price': '70',
+			'brand': 'Koton'
+		},
+		{
+			'productId': 2,
+			'name': 'Pants',
+			'imgUrl': '',
+			'price': '70',
+			'brand': 'Koton'
+		},
+		{
+			'productId': 2,
+			'name': 'Pants',
+			'imgUrl': '',
+			'price': '70',
+			'brand': 'Koton'
+		},
+		{
+			'productId': 2,
+			'name': 'Pants',
+			'imgUrl': '',
+			'price': '70',
+			'brand': 'Koton'
+		}
+	]
+
 	return(
 		<div>
-
+			<div className='row' >
+			{products.length != 0 ? products.map((p) =>
+			
+				<div className='col-sm-6 col-md-4 col-lg-3 mt-4' key={p.productId}>
+					<div className="justify-content-center">
+				<VendorProductCard
+					name={p.name} 
+					price={p.price} 
+					imgUrl={p.imageUrl} 
+					productId={p.productId} 
+					brand={p.brand}
+				/>
+				</div>
+				</div>
+			
+			) :
+			<div className='container p-5'>
+				<p>You haven't added any products.</p>
+			</div>}
+			</div>
 		</div>
 	)
 }
