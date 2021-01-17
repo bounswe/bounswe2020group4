7f9a1f49013cc21f2a1488c1632f67f7ee6fcc4e
Eylul Yalcinkaya <eylulyalcnkaya@gmail.com>
2021-01-18 01:32:19 +0300

    Possible attribute and stock values UI implemented

--- web/src/pages/AddProduct.js
+++ web/src/pages/AddProduct.js
@@ -17,6 +17,11 @@ const AddProduct= (props) => {
 	const [brand, setBrand] = useState('')
 	const [attributes, setAttributes] = useState([])
 	const [attributeSelected, setAttributeSelected] = useState(false)
+	const [possibleValues, setPossibleValues] = useState({})
+	const [rerender, setRerender] = useState(false)
+	const [possibleValuesEntered, setPossibleValuesEntered] = useState(false)
+	const [values, setValues] = useState({})
+	const [productInfos, setProductInfos] = useState([])
 
 
 	useEffect(() => {
@@ -83,6 +88,7 @@ const AddProduct= (props) => {
 		}
 		if(attributes.length){
 			setAttributeSelected(true)
+			setRerender(!rerender)
 		} else {
 			setAttributeSelected(false)
 			alert('You must write at least one criteria first.')
@@ -92,13 +98,60 @@ const AddProduct= (props) => {
 	const handleAttributeAdd = (attribute) => {
 		attributes.push(attribute)
 		setAttributes(attributes)
-		//setAttributeSelected(attributes.length > 0)
 	}
 
 	const handleAttributeDelete = (i) => {
 		attributes.splice(i, 1)
 		setAttributes(attributes)
-		//setAttributeSelected(attributes.length > 0)
+	}
+
+	const handlePossibleValueChange = function(e, attr) {
+		if(e.target.value){
+			possibleValues[attr] = e.target.value.split(',')
+		} else{
+			possibleValues[attr] = ''
+		}
+		setPossibleValues(possibleValues)
+	}
+
+	const handlePossibleValuesButton = function() {
+		for(var value in possibleValues){
+			if(possibleValues[value].length == 0){
+				alert('You have to enter possible values for every criteria.')
+				return
+			}
+		}
+		var results = getCombinations(possibleValues, 0, [], {});
+		console.log(results)
+		setPossibleValuesEntered(true)
+	}
+
+	const handleValueChange = function(e, attr){
+		values[attr] = e.target.value
+		setValues(values)
+	}
+
+	const handleAddButton = function(){
+		productInfos.push(values)
+		setProductInfos(productInfos)
+		setRerender(!rerender)
+	}
+
+	function getCombinations(options, optionIndex, results, current) {
+		var allKeys = Object.keys(options);
+		var optionKey = allKeys[optionIndex];
+	
+		var vals = options[optionKey];
+		for (var i = 0; i < vals.length; i++) {
+			current[optionKey] = vals[i];
+			if (optionIndex + 1 < allKeys.length) {
+				getCombinations(options, optionIndex + 1, results, current);
+			} else {
+				var res = JSON.parse(JSON.stringify(current));
+				results.push(res);
+			}
+		}
+		return results;
 	}
 
 	return (
@@ -133,10 +186,11 @@ const AddProduct= (props) => {
 			</div>
 			{categorySelected ?
 				<div className='container-fluid mt-3 p-3 container-main rounded'>
+					<p className='h3 header-info'>2. Enter product information and criterion</p>
 					<div className='form-group'>
 						<div className='row'>
 							<div className='col-1'>
-								<label hmtlFor='name'>Name</label>
+								<label htmlFor='name'>Name</label>
 							</div>
 							<div className='col-4'>
 								<input type='text' className='form-control form-control-md text-left' id='name' value={productName} onChange={(e)=>setProductName(e.target.value)}/>
@@ -146,7 +200,7 @@ const AddProduct= (props) => {
 					<div className='form-group'>
 						<div className='row'>
 							<div className='col-1'>
-								<label hmtlFor='brand'>Brand</label>
+								<label htmlFor='brand'>Brand</label>
 							</div>
 							<div className='col-4'>
 								<input type='text' className='form-control form-control-md text-left' id='brand' value={brand} onChange={(e)=>setBrand(e.target.value)}/>
@@ -159,7 +213,7 @@ const AddProduct= (props) => {
 								<label htmlFor='description'>Description</label>
 							</div>
 							<div className='col'>
-								<textarea type='text' className='form-control form-control-md text-left' id='surname' value={description} onChange={(e)=>setDescription(e.target.value)}/>
+								<textarea type='text' className='form-control form-control-md text-left' id='description' value={description} onChange={(e)=>setDescription(e.target.value)}/>
 							</div>
 						</div>
 					</div>
@@ -181,7 +235,69 @@ const AddProduct= (props) => {
 				null
 			}
 			{attributeSelected ?
-				<div>hello</div>
+				<div className='container-fluid mt-3 p-3 container-main rounded'>
+					<p className='h3 header-info'>3. Enter possible values for criterion separated by ,</p>
+					{attributes.map((attr) =>
+						<div key={attr.text}>
+							<div className='form-group'>
+								<div className='row'>
+									<div className='col-1'>
+										<label htmlFor={attr.text}>{attr.text}</label>
+									</div>
+									<div className='col-4'>
+										<input type='text' className='form-control form-control-md text-left' 
+										id={attr.text} 
+										onChange={(e) => handlePossibleValueChange(e, attr.text)}
+										/>
+									</div>
+								</div>
+							</div>
+					</div>
+					)}
+					<div className='row'>
+						<div className='col'></div>
+						<div className='col-3 align-self-end text-right mb-6'>
+							<button className="btn btn-danger next-button" onClick={handlePossibleValuesButton}>Next</button>
+						</div>
+					</div>
+				</div>
+					:
+				null
+			}
+			{possibleValuesEntered ?
+				<div className='container-fluid mt-3 p-3 container-main rounded'>
+					<p className='h3 header-info'>4. Enter stock information</p>
+					{attributes.map((attr) =>
+						<div key={attr.text}>
+							<div className='form-group'>
+								<div className='row'>
+									<div className='col-3'>
+										<label htmlFor={attr.text+'stock'}>{attr.text}</label>
+									</div>
+									<div className='col'>
+										<input type='text' className='form-control form-control-md text-left' id={attr.text+'stock'} onChange={(e) => handleValueChange(e, attr.text)}/>
+									</div>
+								</div>
+							</div>
+						</div>
+					)}
+					<div className='form-group'>
+								<div className='row'>
+									<div className='col-3'>
+										<label htmlFor='stock'>Stock</label>
+									</div>
+									<div className='col'>
+										<input type='number' className='form-control form-control-md text-left' id='stock' onChange={(e) => handleValueChange(e, 'stock')}/>
+									</div>
+								</div>
+							</div>
+					<div className='row'>
+						<div className='col'></div>
+						<div className='col-3 align-self-end text-right mb-6'>
+							<button className="btn btn-danger next-button" onClick={handleAddButton}>Add</button>
+						</div>
+					</div>
+				</div>
 					:
 				null
 			}
