6bf9a489d56ae24e0640552ef7046a55cd28c4f0
Berke Can Gürer <berke.gurer@gmail.com>
2021-01-21 22:59:46 +0300

    Added notifications service to get activity stream data from backend

---
+++ web/src/services/notifications.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+const baseUrl = 'http://3.138.113.101:8080/notification'
+
+const getNotifications = async (userType, userId) => {
+	try {
+		const response = await axios.get(`${baseUrl}?userType=${userType}&userId=${userId}`)
+		return response?.data?.data?.items
+	} catch (err) {
+		return null
+	}
+	
+}
+
+export default { getNotifications }
