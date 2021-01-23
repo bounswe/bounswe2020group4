1051c3698e794c5430e935601c704eba73e79d5b
koraycetiin <koraycetin67@gmail.com>
2021-01-23 22:31:25 +0300

    delete all collections in db service change productinfos logic minor bugs

--- backend/db_service/db/init.js
+++ backend/db_service/db/init.js
@@ -1,5 +1,11 @@
 const mongoose = require("mongoose");
 const Product = require("../models/product").Product;
+const Comment = require("../models/comment").Comment;
+const Customer = require("../models/customer").Customer;
+const Like = require("../models/like").Like;
+const Message = require("../models/message").Message;
+const OrderedProduct = require("../models/ordered_product").OrderedProduct;
+const CartProduct = require("../models/cart_products").CartProduct;
 const Vendor = require("../models/vendor").Vendor;
 const fs = require("fs");
 
@@ -10,17 +16,24 @@ const fs = require("fs");
 module.exports.initializeMockDB = async () => {
   await Product.deleteMany();
   await Vendor.deleteMany();
+  await Comment.deleteMany();
+  await Customer.deleteMany();
+  await Like.deleteMany();
+  await Message.deleteMany();
+  await OrderedProduct.deleteMany();
+  await CartProduct.deleteMany();
 
   const clothingProducts = JSON.parse(fs.readFileSync("./mock-product-data/clothing.json"));
   const cosmeticProducts = JSON.parse(fs.readFileSync("./mock-product-data/cosmetic.json"));
   const electronicProducts = JSON.parse(fs.readFileSync("./mock-product-data/electronics.json"));
+  const furnitureProducts = JSON.parse(fs.readFileSync("./mock-product-data/furniture.json"));
   let vendors = JSON.parse(fs.readFileSync("./mock-product-data/vendors.json"));
 
   const res = await Vendor.collection.insertMany(vendors);
 
   vendors = Object.values(res.insertedIds);
 
-  const products = clothingProducts.concat(cosmeticProducts).concat(electronicProducts);
+  const products = clothingProducts.concat(cosmeticProducts).concat(electronicProducts).concat(furnitureProducts);
 
   products.forEach((product) => (product.vendorId = vendors[Math.floor(Math.random() * vendors.length)]));
 
