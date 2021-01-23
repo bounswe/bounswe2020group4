0fce23f620f83c1a33c25b0d6d666362b3fc60a4
eridincu <m.erdincoguz@gmail.com>
2021-01-23 22:25:25 +0300

    Update status

--- backend/checkout_service/constants/error.js
+++ backend/checkout_service/constants/error.js
@@ -4,7 +4,12 @@ const ErrorMessage = {
   MISSING_PARAMETER: "Missing parameter, see documentation for details",
   ADDRESS_ALREADY_EXISTS: "Address with the title already exists",
   USER_NOT_FOUND: "User not found",
-  PRODUCT_NOT_FOUND: "Product not found",
+  PRODUCT_NOT_FOUND: "Product(s) not found",
+  WRONG_USER_TYPE: "Please use one of the following user types: customer, vendor",
+  ALREADY_SHIPPED: "Product(s) cannot be cancelled, it is already shipped.",
+  SHOULD_BE_DELIVERED: "Product(s) should be delivered first to return it.",
+  VENDOR_CANNOT_RETURN: "Vendors cannot return a product.",
+  ALREADY_CANCEL_OR_RETURN: "Product(s) already cancelled or returned.",
 };
 
 module.exports.ErrorCode = (message) => {
@@ -13,6 +18,11 @@ module.exports.ErrorCode = (message) => {
   ErrorCodes[ErrorMessage.EMAIL_HAS_BEEN_USED] = 400;
   ErrorCodes[ErrorMessage.COULD_NOT_CREATE_USER] = 400;
   ErrorCodes[ErrorMessage.MISSING_PARAMETER] = 400;
+  ErrorCodes[ErrorMessage.WRONG_USER_TYPE] = 400;
+  ErrorCodes[ErrorMessage.ALREADY_CANCEL_OR_RETURN] = 400;
+  ErrorCodes[ErrorMessage.SHOULD_BE_DELIVERED] = 400;
+  ErrorCodes[ErrorMessage.VENDOR_CANNOT_RETURN] = 400;
+  ErrorCodes[ErrorMessage.ALREADY_SHIPPED] = 400;
   ErrorCodes[ErrorMessage.USER_NOT_FOUND] = 404;
   ErrorCodes[ErrorMessage.PRODUCT_NOT_FOUND] = 404;
 
--- backend/checkout_service/views/order.js
+++ backend/checkout_service/views/order.js
@@ -2,9 +2,11 @@ const OrderedProduct = require("../models/ordered_product").OrderedProduct;
 const CartProduct = require("../models/cart_products").CartProduct;
 const Product = require("../models/product").Product;
 const Vendor = require("../models/vendor").Vendor;
+const Customer = require("../models/customer").Customer;
 const ObjectId = require("mongoose").Types.ObjectId;
 const { checkCreditCard } = require("./verification");
 const Moment = require("moment");
+const { ErrorMessage } = require("../constants/error");
 // TODO(eridincu): UNAVAILABLE PRODUCTS BEHAVIOUR
 module.exports.checkoutOrder = async (params) => {
   try {
@@ -113,7 +115,7 @@ module.exports.checkoutOrder = async (params) => {
     }
   } catch (error) {
     console.log(error);
-    return error;
+    return { success: false, message: error.message || error };
   }
 };
 module.exports.getOrders = async (params) => {
@@ -122,12 +124,12 @@ module.exports.getOrders = async (params) => {
     if (params.userType === "customer") {
       orderedProducts = await OrderedProduct.find({ customerId: ObjectId(params.id) });
     } else if (params.userType === "vendor") {
-      orderedProducts = await OrderedProduct.find({ vendorId: params.id });
+      orderedProducts = await OrderedProduct.find({ vendorId: ObjectId(params.id) });
     } else {
-      return false;
+      return { success: false, message: ErrorMessage.WRONG_USER_TYPE};
     }
     if (orderedProducts.length === 0) {
-      return false;
+      return { success: false, message: ErrorMessage.PRODUCT_NOT_FOUND};
     }
     let orders = {};
     orderedProducts = await Promise.all(
@@ -139,9 +141,6 @@ module.exports.getOrders = async (params) => {
           date: orderedProduct.date,
           products: [],
         };
-        // get vendor and product information
-        const product = await Product.findById(orderedProduct.productId);
-        const vendor = await Vendor.findById(orderedProduct.vendorId);
         // the product json which returned to client
         let tempOrderedProduct = {
           orderedProductId: orderedProduct._id,
@@ -152,55 +151,233 @@ module.exports.getOrders = async (params) => {
           price: product.price,
           originalPrice: product.originalPrice,
           brand: product.brand,
-          vendor: {
+          quantity: orderedProduct.productInfo.quantity,
+          attributes: orderedProduct.productInfo.attributes,
+        };;
+        // get vendor and product information
+        const product = await Product.findById(orderedProduct.productId);
+        if (params.userType === "customer") {
+          const vendor = await Vendor.findById(orderedProduct.vendorId);
+          // the product json which returned to client
+          tempOrderedProduct.vendor = {
             id: vendor._id,
             name: vendor.name,
             rating: vendor.rating,
-          },
-          quantity: orderedProduct.productInfo.quantity,
-          attributes: orderedProduct.productInfo.attributes,
-        };
-        // update status according to date
-        const orderDate = orderedProduct.date;
-        if (Moment(orderDate).add(5, "days").toDate() < new Date()) {
-          orderedProduct.status = "Approved";
-        } else if (Moment(orderDate).add(3, "days").toDate() < new Date()) {
-          orderedProduct.status = "Delivered at " + Moment(orderDate).add(3, "days").toDate().toString();
-        } else if (Moment(orderDate).add(1, "days").toDate() < new Date()) {
-          orderedProduct.status = "Shipped";
+          };
+        } else {
+          const customer = await Customer.findById(orderedProduct.customerId);
+          tempOrderedProduct.customer = {
+            id: customer._id,
+          };
         }
+        // update status according to date.
+        const orderDate = orderedProduct.date;
+        orderedProduct = updateStatusByDate(orderedProduct, orderDate);
+        // after updating the status, add the product to the list returned to the client.
         tempOrderedProduct.status = orderedProduct.status;
         orders[orderedProduct.orderId].products.push(tempOrderedProduct);
         await orderedProduct.save();
       })
     );
-    return orders;
+    return {success: true, data: orders};
   } catch (error) {
     console.log(error);
-    return error;
+    return { success: false, message: error.message || error };
+  }
+};
+module.exports.updateProductStatus = async (params) => {
+  try {
+    if (!(params.userType && params.status && params.userId && params.productId && params.orderId)) {
+      return { success: false, message: ErrorMessage.MISSING_PARAMETER };
+    }
+    let orderedProduct;
+    if (params.userType == "customer") {
+      orderedProduct = await OrderedProduct.findOne({ orderId: ObjectId(params.orderId), customerId: ObjectId(params.userId), productId: ObjectId(params.productId) });
+    } else if (params.userType == "vendor") {
+      orderedProduct = await OrderedProduct.findOne({ orderId: ObjectId(params.orderId), vendorId: ObjectId(params.userId), productId: ObjectId(params.productId) });
+    } else {
+      return { success: false, message: ErrorMessage.WRONG_USER_TYPE };
+    }
+    // Check if product is found.
+    if (!orderedProduct) {
+      return { success: false, message: ErrorMessage.PRODUCT_NOT_FOUND };
+    }
+    // Check if vendor is trying to return a product.
+    if (params.status = "Returned" && params.userType == "vendor") {
+      return { success: false, message: ErrorMessage.VENDOR_CANNOT_RETURN };
+    }
+    // Product is already cancelled or returned.
+    if (orderedProduct.status == "Cancelled" || orderedProduct.status == "Returned") {
+      return { success: false, message: ErrorMessage.ALREADY_CANCEL_OR_RETURN };
+    }
+    // Check if cancelling that product is valid.
+    if (params.status == "Cancelled") {
+      if (Moment(orderedProduct.date).add(1, "days").toDate() < new Date()) {
+        return { success: false, message: ErrorMessage.ALREADY_SHIPPED };
+      } else {
+        // TODO(eridincu): Update stock value of the product in the product database.
+        let db_product = await Product.findById(orderedProduct.productId);
+        if (!db_product) {
+          return { success: false, message: ErrorMessage.PRODUCT_NOT_FOUND };
+        }
+        // update the stock value of the product in product DB 
+        await Promise.all(
+          db_product.productInfos.map(async (productInfo) => {
+            // find the correct productInfo of the product in the product database
+            let isCorrectProductInfo = true;
+            productInfo.attributes.forEach((attr) => {
+              orderedProduct.productInfo.attributes.forEach((order_attr) => {
+                if (attr.name === order_attr.name) {
+                  if (attr.value !== order_attr.value) {
+                    isCorrectProductInfo = false;
+                    return;
+                  }
+                }
+              });
+              if (!isCorrectProductInfo) {
+                return;
+              }
+            });
+            // update the stock value of the product in product DB 
+            if (isCorrectProductInfo) {
+              productInfo.stockValue += orderedProduct.productInfo.quantity;
+              await db_product.save();
+            }
+          })
+        );
+
+        orderedProduct.status = params.status;
+        await orderedProduct.save();
+      }
+    } else if (params.status == "Returned") {
+      // Check if the product can be returned.
+        if (!(Moment(orderedProduct.date).add(3, "days").toDate() < new Date())) {
+          return { succes: false, message: ErrorMessage.SHOULD_BE_DELIVERED };
+        } else {
+          let db_product = await Product.findById(orderedProduct.productId);
+          if (!db_product) {
+            return { success: false, message: ErrorMessage.PRODUCT_NOT_FOUND };
+          }
+          // update the stock value of the product in product DB 
+          await Promise.all(
+            db_product.productInfos.map(async (productInfo) => {
+              // find the correct productInfo of the product in the product database
+              let isCorrectProductInfo = true;
+              productInfo.attributes.forEach((attr) => {
+                orderedProduct.productInfo.attributes.forEach((order_attr) => {
+                  if (attr.name === order_attr.name) {
+                    if (attr.value !== order_attr.value) {
+                      isCorrectProductInfo = false;
+                      return;
+                    }
+                  }
+                });
+                if (!isCorrectProductInfo) {
+                  return;
+                }
+              });
+              // update the stock value of the product in product DB 
+              if (isCorrectProductInfo) {
+                productInfo.stockValue += orderedProduct.productInfo.quantity;
+                await db_product.save();
+              }
+            })
+          );
+          orderedProduct.status = params.status;
+          await orderedProduct.save();
+        }
+    } else {
+      orderedProduct.status = params.status;
+      await orderedProduct.save();
+    }
+    return { success: true };
+  } catch (error) {
+    console.log(error);
+    return { success: false, message: error.message || error }
   }
 };
 module.exports.updateOrderStatus = async (params) => {
   try {
 	if (!(params.userType && params.status && params.orderId && params.userId)) {
-		return false;
+		return { success: false, message: ErrorMessage.MISSING_PARAMETER };
 	}
 	let orderedProducts = ""
 	if (params.userType === "customer") {
 		orderedProducts = await OrderedProduct.find({ orderId: ObjectId(params.orderId), customerId: ObjectId(params.userId) });
 	} else {
 		orderedProducts = await OrderedProduct.find({ orderId: ObjectId(params.orderId), vendorId: ObjectId(params.userId) });
-	}
+  }
+  if (orderedProducts.length === 0) {
+    return { success: false, message: ErrorMessage.PRODUCT_NOT_FOUND };
+  }
+  if (params.status === "Cancelled") {
+    if (Moment(params.date).add(1, "days").toDate() < new Date()) {
+      return { success: false, message: ErrorMessage.ALREADY_SHIPPED }; 
+    }
+  } else if (params.status === "Returned") {
+    if (!(Moment(product.date).add(3, "days").toDate() < new Date())) {
+      return { succes: false, message: ErrorMessage.SHOULD_BE_DELIVERED };
+    }
+  }
 	orderedProducts = await Promise.all(
-		orderedProducts.map(async (product) => {
-			product.status = params.status;
-			await product.save();
-			return product;
+		orderedProducts.map(async (orderedProduct) => {
+      let db_product = await Product.findById(orderedProduct.productId);
+      if (!db_product) {
+        return { success: false, message: ErrorMessage.PRODUCT_NOT_FOUND };
+      }
+      // Stock value of cancelled and returned products should be updated. 
+      if (params.status === "Returned" || params.status === "Cancelled") {
+        // update the stock value of the product in product DB 
+        await Promise.all(
+          db_product.productInfos.map(async (productInfo) => {
+            // find the correct productInfo of the product in the product database
+            let isCorrectProductInfo = true;
+            productInfo.attributes.forEach((attr) => {
+              orderedProduct.productInfo.attributes.forEach((order_attr) => {
+                if (attr.name === order_attr.name) {
+                  if (attr.value !== order_attr.value) {
+                    isCorrectProductInfo = false;
+                    return;
+                  }
+                }
+              });
+              if (!isCorrectProductInfo) {
+                return;
+              }
+            });
+            // update the stock value of the product in product DB 
+            if (isCorrectProductInfo) {
+              productInfo.stockValue += orderedProduct.productInfo.quantity;
+              await db_product.save();
+            }
+          })
+        );
+      }
+      orderedProduct.status = params.status;
+      await orderedProduct.save();
+			return orderedProduct;
 		})
 	);
-	return true;
+	return { success: true };
   } catch (error) {
     console.log(error);
-    return error;
+    return { success: false, message: error.message || error };
   }
 };
+// Check if the product is cancelled before; if not, update according to the date.
+function updateStatusByDate(product, date) {
+  try {
+    if (product.status == "Cancelled" || product.status == "Returned") {
+      // DO NOTHING
+    } else if (Moment(date).add(5, "days").toDate() < new Date()) {
+      product.status = "Approved";
+    } else if (Moment(date).add(3, "days").toDate() < new Date()) {
+      product.status = "Delivered at " + Moment(date).add(3, "days").toDate().toString();
+    } else if (Moment(date).add(1, "days").toDate() < new Date()) {
+      product.status = "Shipped";
+    }
+    return product;
+  } catch (error) {
+    console.log(error);
+  }
+}
