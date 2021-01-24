ac347086b627484da1b2a8b0b04fd7b4cbb29361
koraycetiin <koraycetin67@gmail.com>
2021-01-24 14:50:59 +0300

    add notiifcation in vendor view

--- backend/vendor_service/routes/vendor.js
+++ backend/vendor_service/routes/vendor.js
@@ -2,16 +2,15 @@ const file = require("../views/file");
 const vendor = require("../views/vendor");
 const { ErrorCode } = require("../constants/error");
 
-
 module.exports.initialize = (app) => {
   app.post("/vendor/products", async (request, response) => {
     const result = await vendor.addProducts(request.body);
     if (result["idList"]) {
       response.respond(200, "OK", {
-        result
+        result,
       });
     } else {
-      response.respond(400,"Please check your products' information");
+      response.respond(400, "Please check your products' information");
     }
   });
 
@@ -21,68 +20,55 @@ module.exports.initialize = (app) => {
     response.respond(200, "OK", { urls });
   });
 
-
   app.patch("/vendor/products/:vendorId", async (request, response) => {
-    var productId = request.params.id
+    var productId = request.params.id;
     var changeParameters = request.body;
 
-    const result = await vendor.updateProduct(productId,changeParameters);
+    const result = await vendor.updateProduct(productId, changeParameters);
 
     if (!!result.name) {
       response.respond(200, "OK", {
         result,
       });
-    } 
-    else {
+    } else {
       response.respond(400, result);
     }
   });
 
-
-
   app.get("/vendor/products/:vendorId", async (request, response) => {
-    parameters = request.body
-    parameters["vendorId"]  = request.params.vendorId
+    parameters = request.body;
+    parameters["vendorId"] = request.params.vendorId;
 
     const result = await vendor.getProducts(parameters);
-    if ( !!result.success ) {
+    if (!!result.success) {
       response.respond(ErrorCode(result.message), result.message);
-    }
-    else if (!!result.productList) {
+    } else if (!!result.productList) {
       response.respond(200, "OK", {
         result,
       });
-    } 
-    else {
+    } else {
       response.respond(ErrorCode(result.message), result.message);
     }
   });
 
-
   app.get("/vendor/vendorlist", async (request, response) => {
-
     const result = await vendor.getVendorList();
     response.respond(200, "OK", {
-      result
-    });   
-
+      result,
+    });
   });
 
-
-
-  app.delete("/vendor/product/:vendorId", async (request, response) => {    
+  app.delete("/vendor/product/:vendorId", async (request, response) => {
     var parameter = request.body;
-    parameter["vendorId"] =  request.params.vendorId
+    parameter["vendorId"] = request.params.vendorId;
 
     const result = await vendor.deleteProduct(parameter);
 
-    console.log(result)
-    if(result){
-      response.respond(200, "The product is deleted successfully");   
-    }else {
-      response.respond(400, "Please check your product information. It has never existed or been already deleted");   
+    console.log(result);
+    if (result) {
+      response.respond(200, "The product is deleted successfully");
+    } else {
+      response.respond(400, "Please check your product information. It has never existed or been already deleted");
     }
-
   });
-
 };
