e521be0cd069e4c4099d114face3cf6f7dccfc91
burak <burakcuhadar96@gmail.com>
2021-01-25 22:05:37 +0300

    Fix message displaying bug

--- web/src/services/orders.js
+++ web/src/services/orders.js
@@ -5,7 +5,6 @@ const baseUrl = 'http://3.138.113.101:8080/order'
 const getOrders = async (userId, userType) => {
 	try {
 		const response = await axios.get(`${baseUrl}?id=${userId}&userType=${userType}`)
-		console.log(response)
 		return response.data.data
 	} catch(err) {
 		return []
